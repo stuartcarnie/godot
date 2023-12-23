438e81092d1fd27dd5f1e4cfa85065c90c6ecb71
Stuart Carnie <stuart.carnie@gmail.com>
2023-12-24 08:19:12 +1100

    fix: image atomics not supported by Metal
    Separate by feature. Metal does not support image atomics, but it does
    support subgroups

--- servers/rendering/renderer_rd/shader_rd.cpp
+++ servers/rendering/renderer_rd/shader_rd.cpp
@@ -192,8 +192,12 @@ void ShaderRD::_build_variant_code(StringBuilder &builder, uint32_t p_variant, c
 					builder.append(String("#define ") + String(E.key) + "_CODE_USED\n");
 				}
 #if defined(MACOS_ENABLED) || defined(IOS_ENABLED)
-				builder.append("#define MOLTENVK_USED\n");
+				if (RD::get_singleton()->get_device_capabilities()->device_family == RD::DeviceFamily::DEVICE_VULKAN) {
+					builder.append("#define MOLTENVK_USED\n");
+				}
 #endif
+				builder.append("#define NO_IMAGE_ATOMICS\n");
+
 				builder.append(String("#define RENDER_DRIVER_") + OS::get_singleton()->get_current_rendering_driver_name().to_upper() + "\n");
 			} break;
 			case StageTemplate::Chunk::TYPE_MATERIAL_UNIFORMS: {
