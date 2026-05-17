bedd343f09dd56506b7199c8b7ef95668eab71f9
mikael <mikaellegoff@gmail.com>
2026-05-17 18:49:23 +0900

    Add tvOS Siri Remote input bridge

--- platform/tvos/godot_view_tvos.mm
+++ platform/tvos/godot_view_tvos.mm
@@ -31,6 +31,7 @@
 #import "godot_view_tvos.h"
 
 #import "display_layer_tvos.h"
+#import "display_server_tvos.h"
 
 #include "core/error/error_macros.h"
 
@@ -51,6 +52,138 @@ GODOT_CLANG_WARNING_POP
 - (void)godot_commonInit {
 	[super godot_commonInit];
 	self.contentScaleFactor = 1.0;
+	self.userInteractionEnabled = YES;
+
+	UISwipeGestureRecognizerDirection directions[] = {
+		UISwipeGestureRecognizerDirectionLeft,
+		UISwipeGestureRecognizerDirectionRight,
+		UISwipeGestureRecognizerDirectionUp,
+		UISwipeGestureRecognizerDirectionDown,
+	};
+	for (int i = 0; i < 4; i++) {
+		UISwipeGestureRecognizer *swipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(godot_remoteSwipe:)];
+		swipe.direction = directions[i];
+		[self addGestureRecognizer:swipe];
+	}
+}
+
+- (BOOL)canBecomeFocused {
+	return YES;
+}
+
+- (void)didMoveToWindow {
+	[super didMoveToWindow];
+	if (self.window) {
+		[self setNeedsFocusUpdate];
+		[self updateFocusIfNeeded];
+	}
+}
+
+- (void)godot_sendRemoteKey:(Key)key pressed:(BOOL)pressed {
+	DisplayServerTVOS *display_server = DisplayServerTVOS::get_singleton();
+	if (!display_server) {
+		return;
+	}
+	display_server->key(key, 0, key, key, 0, pressed, KeyLocation::UNSPECIFIED);
+}
+
+- (void)godot_tapRemoteKey:(Key)key {
+	[self godot_sendRemoteKey:key pressed:YES];
+	[self godot_sendRemoteKey:key pressed:NO];
+}
+
+- (void)godot_remoteSwipe:(UISwipeGestureRecognizer *)sender {
+	switch (sender.direction) {
+		case UISwipeGestureRecognizerDirectionLeft:
+			[self godot_tapRemoteKey:Key::LEFT];
+			break;
+		case UISwipeGestureRecognizerDirectionRight:
+			[self godot_tapRemoteKey:Key::RIGHT];
+			break;
+		case UISwipeGestureRecognizerDirectionUp:
+			[self godot_tapRemoteKey:Key::UP];
+			break;
+		case UISwipeGestureRecognizerDirectionDown:
+			[self godot_tapRemoteKey:Key::DOWN];
+			break;
+		default:
+			break;
+	}
+}
+
+- (void)pressesBegan:(NSSet<UIPress *> *)presses withEvent:(UIPressesEvent *)event {
+	BOOL handled = NO;
+	for (UIPress *press in presses) {
+		switch (press.type) {
+			case UIPressTypeSelect:
+				[self godot_sendRemoteKey:Key::ENTER pressed:YES];
+				handled = YES;
+				break;
+			case UIPressTypeMenu:
+				[self godot_sendRemoteKey:Key::ESCAPE pressed:YES];
+				handled = YES;
+				break;
+			case UIPressTypePlayPause:
+				[self godot_sendRemoteKey:Key::SPACE pressed:YES];
+				handled = YES;
+				break;
+			default:
+				break;
+		}
+	}
+	if (!handled) {
+		[super pressesBegan:presses withEvent:event];
+	}
+}
+
+- (void)pressesEnded:(NSSet<UIPress *> *)presses withEvent:(UIPressesEvent *)event {
+	BOOL handled = NO;
+	for (UIPress *press in presses) {
+		switch (press.type) {
+			case UIPressTypeSelect:
+				[self godot_sendRemoteKey:Key::ENTER pressed:NO];
+				handled = YES;
+				break;
+			case UIPressTypeMenu:
+				[self godot_sendRemoteKey:Key::ESCAPE pressed:NO];
+				handled = YES;
+				break;
+			case UIPressTypePlayPause:
+				[self godot_sendRemoteKey:Key::SPACE pressed:NO];
+				handled = YES;
+				break;
+			default:
+				break;
+		}
+	}
+	if (!handled) {
+		[super pressesEnded:presses withEvent:event];
+	}
+}
+
+- (void)pressesCancelled:(NSSet<UIPress *> *)presses withEvent:(UIPressesEvent *)event {
+	BOOL handled = NO;
+	for (UIPress *press in presses) {
+		switch (press.type) {
+			case UIPressTypeSelect:
+				[self godot_sendRemoteKey:Key::ENTER pressed:NO];
+				handled = YES;
+				break;
+			case UIPressTypeMenu:
+				[self godot_sendRemoteKey:Key::ESCAPE pressed:NO];
+				handled = YES;
+				break;
+			case UIPressTypePlayPause:
+				[self godot_sendRemoteKey:Key::SPACE pressed:NO];
+				handled = YES;
+				break;
+			default:
+				break;
+		}
+	}
+	if (!handled) {
+		[super pressesCancelled:presses withEvent:event];
+	}
 }
 
 - (CALayer<GDTDisplayLayer> *)initializeRenderingForDriver:(NSString *)driverName {
